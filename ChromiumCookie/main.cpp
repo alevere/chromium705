ee6981cdbac95a5ec42c81230108ea348dce578b
alevere <alevere@fishs-MacBook-Pro.local>
2015-04-05 17:55:37 -0400

    Initial Commit

---
+++ ChromiumCookie/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ChromiumCookie
+//
+//  Created by alevere on 4/5/15.
+//  Copyright (c) 2015 alevere. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
